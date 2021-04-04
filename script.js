d8136079e8327cfd853bcf30529afa0092a27172
Fortis <fortispotens@gmail.com>
2021-04-04 04:35:28 +0100

    setup JavaScript and number button clicks and display on screen

---
+++ script.js
@@ -0,0 +1,33 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector(".calc-screen");
+
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+}
+function handleSymbol(symbol) {}
+
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+  screen.innerText = buffer;
+}
+
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
+}
+
+init();
